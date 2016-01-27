7526bb042ffdb5fd858b46e6b8ec37a6c4140ea9
DiMesq <d.mesquita285@gmail.com>
2016-01-27 13:20:05 +0000

    Adds holder for the main page template.

---
+++ template/home.php
@@ -0,0 +1,30 @@
+<header class="jumbotron">
+  <div class="container">
+      <div class="row row-header">
+          <div class="col-xs-12 col-sm-8">
+              <h1>Diogo Mesquita</h1>
+              <p style="padding:40px;"></p>
+              <p>Computer Science student from Portugal</p>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+          </div>
+      </div>
+  </div>
+</header>
+<div class="container">
+
+  <div class="row row-content">
+      <div class="col-xs-12 col-sm-3 col-sm-push-9">
+          <p style="padding:20px;"></p>
+          <h3 align=center>Yes, I know</h3>
+      </div>
+      <div class="col-xs-12 col-sm-9 col-sm-pull-3">
+          <h2>About Me</h2>
+          <p>Love pizza.</p>
+          <p><a  href="#">More &raquo;</a></p>
+      </div>
+  </div>
+
+</div>
+
+    
