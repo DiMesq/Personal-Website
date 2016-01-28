1d747771d3a39e4b047319d88375b7158d495b20
DiMesq <d.mesquita285@gmail.com>
2016-01-28 11:21:19 +0000

    Changes how the home page will be displayed. Adds a side-navbar.

--- template/home.php
+++ template/home.php
@@ -1,30 +1,42 @@
 <header class="jumbotron">
   <div class="container">
-      <div class="row row-header">
-          <div class="col-xs-12 col-sm-8">
-              <h1>Diogo Mesquita</h1>
-              <p style="padding:40px;"></p>
-              <p>Computer Science student from Portugal</p>
-          </div>
-          <div class="col-xs-12 col-sm-4">
-          </div>
+    <div class="row row-header">
+      <div class="col-xs-12 col-sm-8">
+        <h1>Diogo Mesquita</h1>
+        <p style="padding:10px;"></p>
+        <p>Computer Science student</p>
       </div>
+      <div class="col-xs-12 col-sm-4">
+        <div><img src="img/dim.png"></div>
+      </div>
+    </div>
   </div>
 </header>
 <div class="container">
-
-  <div class="row row-content">
-      <div class="col-xs-12 col-sm-3 col-sm-push-9">
-          <p style="padding:20px;"></p>
-          <h3 align=center>Yes, I know</h3>
+<div class="row">
+  <div class="col-sm-3">
+    <div class="sidebar-nav">
+      <div class="navbar navbar-default" role="navigation">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <span class="visible-xs navbar-brand">Sidebar menu</span>
+        </div>
+        <div class="navbar-collapse collapse sidebar-navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">About</a></li>
+            <li><a href="#">Current activities</a></li>
+            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects</a></li>
+            <li><a href="#">Past</a></li>
+            <li><a href="#">Other</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-      <div class="col-xs-12 col-sm-9 col-sm-pull-3">
-          <h2>About Me</h2>
-          <p>Love pizza.</p>
-          <p><a  href="#">More &raquo;</a></p>
-      </div>
+    </div>
   </div>
-
-</div>
-
+  <div class="col-sm-9"></div>
     
