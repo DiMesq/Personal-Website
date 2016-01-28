3eeeada34b1c2fbf33d5a95be22d8466d738900a
DiMesq <d.mesquita285@gmail.com>
2016-01-28 21:43:48 +0000

    Adds a small shadow to the text and image.

--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,10 @@
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-  </body>
+    <script src="js/collapse_from_navbar.js" type="text/javascript"></script>
+</body>
 </html>
 
 
